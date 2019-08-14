991fecdea4deb900efbe448245b64c4ef82057a3
Sreeja Paluri <sreeja.paluri@hcl.com>
2019-08-13 22:58:46 -0400

    section 3

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 
-import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/addProject" component={AddProject} />
+            <Route exact path="/Dashboard" component={Dashboard} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ ppmtool-react-client/src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+
+import errorReducer from "./errorReducer";
+
+import projectReducer from "./projectReducer";
+
+export default combineReducers({
+  errors: errorReducer,
+
+  project: projectReducer
+});
